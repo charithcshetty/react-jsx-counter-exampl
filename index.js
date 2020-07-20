9a2fb89bcacc2340bdae2073bb7bbf96aaa392e0
charithcshetty <64842641+charithcshetty@users.noreply.github.com>
2020-07-20 12:43:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 let counter=0
-
+function show(){
+  counter++;
+  const el=<p>{counter}</p>
+  ReactDOM.render(
+    el,document.getElementById("root")
+  )
+};
+setInterval(show,1000);
 
